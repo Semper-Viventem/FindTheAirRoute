0da76fbaff9710d30fa09071b6005c376ff0af49
Constantine <kostyaxxx8@gmail.com>
2019-05-29 01:40:17 +0300

    Create marker animation logic

--- app/src/main/java/ru/semper_viventem/findtheairroute/extensions/AppExtension.kt
+++ app/src/main/java/ru/semper_viventem/findtheairroute/extensions/AppExtension.kt
@@ -4,3 +4,16 @@ import com.google.android.gms.maps.model.LatLng
 import ru.semper_viventem.findtheairroute.domain.Location
 
 fun Location.toLatLng() = LatLng(lat, lon)
+
+fun LatLng.distanceTo(latLng: LatLng): Float {
+    val from = android.location.Location("").apply {
+        this.latitude = this@distanceTo.latitude
+        this.longitude = this@distanceTo.longitude
+    }
+    val to = android.location.Location("").apply {
+        this.latitude = latLng.latitude
+        this.longitude = latLng.longitude
+    }
+
+    return from.distanceTo(to)
+}
--- app/src/main/java/ru/semper_viventem/findtheairroute/ui/map/ResultScreen.kt
+++ app/src/main/java/ru/semper_viventem/findtheairroute/ui/map/ResultScreen.kt
@@ -1,8 +1,11 @@
 package ru.semper_viventem.findtheairroute.ui.map
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.animation.addListener
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -12,9 +15,11 @@ import org.koin.android.ext.android.getKoin
 import ru.semper_viventem.findtheairroute.R
 import ru.semper_viventem.findtheairroute.domain.City
 import ru.semper_viventem.findtheairroute.domain.Location
+import ru.semper_viventem.findtheairroute.extensions.distanceTo
 import ru.semper_viventem.findtheairroute.extensions.toLatLng
 import ru.semper_viventem.findtheairroute.ui.UIModule
 import ru.semper_viventem.findtheairroute.ui.common.MapScreen
+
 
 class ResultScreen : MapScreen<ResultPm>() {
 
@@ -32,6 +37,7 @@ class ResultScreen : MapScreen<ResultPm>() {
     override val layoutRes: Int = R.layout.screen_result
 
     override fun provideMapFragmentId(view: View): MapView = view.map
+    private var airplaneAnimator: Animator? = null
 
     override fun providePresentationModel(): ResultPm = getKoin()
         .apply {
@@ -61,7 +67,8 @@ class ResultScreen : MapScreen<ResultPm>() {
             val cameraOffset = resources.getDimensionPixelOffset(R.dimen.normal_gap)
             googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, cameraOffset))
 
-            drawRoute(googleMap, fromCity.location.toLatLng(), toCity.location.toLatLng())
+            val route = drawRoute(googleMap, fromCity.location.toLatLng(), toCity.location.toLatLng())
+            drawAnimation(googleMap, route)
         }
     }
 
@@ -73,16 +80,95 @@ class ResultScreen : MapScreen<ResultPm>() {
         return map.addMarker(marker)
     }
 
-    private fun drawRoute(map: GoogleMap, from: LatLng, to: LatLng) {
+    private fun drawRoute(map: GoogleMap, from: LatLng, to: LatLng): Polyline {
         val strokeWidth = resources.getDimensionPixelOffset(R.dimen.route_stroke_width).toFloat()
         val strokeInterval = resources.getDimensionPixelOffset(R.dimen.route_stroke_interval).toFloat()
-        val poline = PolylineOptions()
-            .add(from, to)
+        val polyline = PolylineOptions()
+            .add(from, LatLng(0.0, 0.0), to)
             .width(strokeWidth)
+            .jointType(JointType.ROUND)
             .color(Color.GRAY)
             .geodesic(true)
             .pattern(listOf(Gap(strokeInterval), Dash(strokeInterval)))
 
-        map.addPolyline(poline)
+        return map.addPolyline(polyline)
+    }
+
+    private fun drawAnimation(map: GoogleMap, polyline: Polyline) {
+        val points = polyline.points
+
+        val airplane = map.addMarker(
+            MarkerOptions()
+                .position(points.first())
+                .anchor(0.5F, 0.5F)
+        )
+
+        val intervalsDuration = getIntervalsDuration(points, 5000)
+
+        animateNextInterval(airplane, points, intervalsDuration, 0)
+    }
+
+    private fun animateNextInterval(airplane: Marker, points: List<LatLng>, intervalDurations: List<Long>, intervalNumber: Int) {
+
+        if (intervalNumber == points.lastIndex) return
+
+        val startPosition = points[intervalNumber]
+        val endPosition = points[intervalNumber + 1]
+
+        airplaneAnimator?.cancel()
+        airplaneAnimator = null
+        airplaneAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            duration = intervalDurations[intervalNumber]
+            addUpdateListener {
+                handleTact(airplane, startPosition, endPosition, it.animatedValue as Float)
+            }
+            addListener(onEnd = {
+                animateNextInterval(airplane, points, intervalDurations, intervalNumber + 1)
+            })
+            start()
+        }
+    }
+
+    private fun handleTact(marker: Marker, startPosition: LatLng, endPosition: LatLng, v: Float) {
+        val lng = v * endPosition.longitude + (1 - v) * startPosition.longitude
+        val lat = v * endPosition.latitude + (1 - v) * startPosition.latitude
+        val newPos = LatLng(lat, lng)
+        marker.position = newPos
+        marker.rotation = getBearing(marker.position, endPosition)
+    }
+
+    private fun getIntervalsDuration(points: List<LatLng>, fullDuration: Long): List<Long> {
+        val distancies = mutableListOf<Float>()
+        points.forEachIndexed { index, latLng ->
+            if (index != points.lastIndex) {
+                distancies.add(latLng.distanceTo(points[index + 1]))
+            }
+        }
+
+        val total = distancies.sum()
+
+        val result = mutableListOf<Long>()
+
+        distancies.forEach { part ->
+            val weight = fullDuration / total * part
+            result.add(weight.toLong())
+        }
+
+        return result
+    }
+
+    private fun getBearing(begin: LatLng, end: LatLng): Float {
+        val lat = Math.abs(begin.latitude - end.latitude)
+        val lng = Math.abs(begin.longitude - end.longitude)
+
+        if (begin.latitude < end.latitude && begin.longitude < end.longitude)
+            return Math.toDegrees(Math.atan(lng / lat)).toFloat()
+        else if (begin.latitude >= end.latitude && begin.longitude < end.longitude)
+            return (90 - Math.toDegrees(Math.atan(lng / lat)) + 90).toFloat()
+        else if (begin.latitude >= end.latitude && begin.longitude >= end.longitude)
+            return (Math.toDegrees(Math.atan(lng / lat)) + 180).toFloat()
+        else if (begin.latitude < end.latitude && begin.longitude >= end.longitude)
+            return (90 - Math.toDegrees(Math.atan(lng / lat)) + 270).toFloat()
+        return -1f
     }
 }

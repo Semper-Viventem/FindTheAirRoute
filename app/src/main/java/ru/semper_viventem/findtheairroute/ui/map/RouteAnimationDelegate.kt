ccbbc547013cd3205a4d177b62204aa968ebbf13
Constantine <kostyaxxx8@gmail.com>
2019-05-29 04:07:25 +0300

    Two-way flying

--- app/src/main/java/ru/semper_viventem/findtheairroute/ui/map/RouteAnimationDelegate.kt
+++ app/src/main/java/ru/semper_viventem/findtheairroute/ui/map/RouteAnimationDelegate.kt
@@ -8,7 +8,6 @@ import androidx.core.animation.addListener
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
 import ru.semper_viventem.findtheairroute.R
-import ru.semper_viventem.findtheairroute.extensions.distanceTo
 import ru.semper_viventem.findtheairroute.ui.common.LatLngBezierInterpolator
 
 class RouteAnimationDelegate(
@@ -62,35 +61,42 @@ class RouteAnimationDelegate(
                 .anchor(0.5F, 0.5F)
         )
 
-        val intervalsDuration = getIntervalsDuration(points, animationDuration)
+        val intervalDuration = animationDuration / points.count()
 
-        animateNextInterval(airplane, points, intervalsDuration, 0)
+        animateNextInterval(airplane, points, intervalDuration, 0, false)
     }
 
-    private fun animateNextInterval(airplane: Marker, points: List<LatLng>, intervalDurations: List<Long>, intervalNumber: Int) {
+    private fun animateNextInterval(airplane: Marker, points: List<LatLng>, intervalDuration: Long, intervalNumber: Int, isReverse: Boolean) {
 
         airplaneAnimator?.cancel()
         airplaneAnimator = null
+        val nextIsReverse = if (intervalNumber == 0 && isReverse) {
+            false
+        } else if (intervalNumber == points.lastIndex && !isReverse) {
+            true
+        } else {
+            isReverse
+        }
+        val nextIntervalNumber = if (nextIsReverse) intervalNumber - 1 else intervalNumber + 1
 
-        if (intervalNumber == points.lastIndex) return
+        val currentPosition = points[intervalNumber]
+        val nextPosition = points[nextIntervalNumber]
 
-        val startPosition = points[intervalNumber]
-        val endPosition = points[intervalNumber + 1]
         airplaneAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
-            duration = intervalDurations[intervalNumber]
+            duration = intervalDuration
             addUpdateListener {
-                handleTact(airplane, startPosition, endPosition, it.animatedValue as Float)
+                handleTact(airplane, currentPosition, nextPosition, it.animatedValue as Float)
             }
             addListener(onEnd = {
-                animateNextInterval(airplane, points, intervalDurations, intervalNumber + 1)
+                animateNextInterval(airplane, points, intervalDuration, nextIntervalNumber, nextIsReverse)
             })
             start()
         }
     }
 
-    private fun handleTact(marker: Marker, startPosition: LatLng, endPosition: LatLng, v: Float) {
-        val lng = v * endPosition.longitude + (1 - v) * startPosition.longitude
-        val lat = v * endPosition.latitude + (1 - v) * startPosition.latitude
+    private fun handleTact(marker: Marker, startPosition: LatLng, endPosition: LatLng, value: Float) {
+        val lng = value * endPosition.longitude + (1 - value) * startPosition.longitude
+        val lat = value * endPosition.latitude + (1 - value) * startPosition.latitude
         val newPos = LatLng(lat, lng)
         marker.position = newPos
         marker.rotation = getBearing(marker.position, endPosition)
@@ -111,26 +117,6 @@ class RouteAnimationDelegate(
         } else 0F
 
         return bearing - 90
-    }
-
-    private fun getIntervalsDuration(points: List<LatLng>, fullDuration: Long): List<Long> {
-        val distancies = mutableListOf<Float>()
-        points.forEachIndexed { index, latLng ->
-            if (index != points.lastIndex) {
-                distancies.add(latLng.distanceTo(points[index + 1]))
-            }
-        }
-
-        val total = distancies.sum()
-
-        val result = mutableListOf<Long>()
-
-        distancies.forEach { part ->
-            val weight = fullDuration / total * part
-            result.add(weight.toLong())
-        }
-
-        return result
     }
 
     private fun getBezierCurvePoints(from: LatLng, to: LatLng): List<LatLng> {
